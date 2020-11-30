af9ad91ae74046736728557503cfe14765a9da4e
enghausen <morten@enghausen.dk>
2020-11-30 13:19:27 +0100

    Update lowVar.sh

--- debian/lowVar.sh
+++ debian/lowVar.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Get low var (0.000-0.001) on Debian 10. Testet on Intel Xeon E3-1246V3 and Intel Core i7-4770.
 # This will install the Real-Time kernel and tuned with the latency-performance profile.
+# Remember to reboot to the new kernel after the installation.
 sudo apt-get update
 sudo apt-get install tuned
 sudo tuned-adm latency-performance
