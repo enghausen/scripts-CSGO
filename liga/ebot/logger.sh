ef389f2a632cbf8cc862ee4a3b77eb98944b419a
enghausen <morten@enghausen.dk>
2020-12-01 12:20:22 +0100

    Update logger.sh

--- liga/ebot/logger.sh
+++ liga/ebot/logger.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-multitail -EX "Completed GOTV demo \"(.*?)\"" "echo >> complete.txt" /home/cs/liga/log/console/1-console.log -I /home/cs/liga/log/console/2-console.log
+multitail -EX "Completed GOTV demo \"(.*?)\"" "xargs ./uploader.sh" /home/cs/liga/log/console/1-console.log -I /home/cs/liga/log/console/2-console.log
