33c8f80c76637adc7ad57006df64ec1ad8155e0e
enghausen <morten@enghausen.dk>
2020-12-02 12:28:00 +0100

    Update uploader.sh

--- liga/ebot/uploader.sh
+++ liga/ebot/uploader.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-curl -F "file=@/home/cs/liga/serverfiles/csgo/$1" https://ebot.doamin.com:12360/upload
-./cleaner.sh $1
+source ./config.sh
+DEMOFILE=$1
+curl -F "file=@$CSGOPATH/$1" https://$DOMAIN:12360/upload
+UPLOADCHECK=$(upload_check $DEMOFILE)
+
+if [ $UPLOADCHECK == 1 ]; then
+  echo "$TIMESTAMP Upload failed for $DEMOFILE" >> $LOGFILE
+elif [ $UPLOADCHECK == 0 ]; then
+  echo "$TIMESTAMP Upload completed for $DEMOFILE" >> $LOGFILE
+else
+  echo "$TIMESTAMP Not sure why I ended up here! File: uploader.sh Demofile: $DEMOFILE and Uploadcheck: $UPLOADCHECK" >> $LOGAFILE
+fi
