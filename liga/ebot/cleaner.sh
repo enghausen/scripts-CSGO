3b2d8d8d8394cb85d0df818480f66e2877df1cc9
enghausen <morten@enghausen.dk>
2020-12-02 12:26:45 +0100

    Update cleaner.sh

--- liga/ebot/cleaner.sh
+++ liga/ebot/cleaner.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-demofile=$1
-matchid=${demofile%%_*}
-uploadCheck=$(curl -v --silent https://ebot.doamin.com/matchs/demo/$matchid 2>&1 | grep -oh $demofile.zip)
+source ./config.sh
+DEMOFILE=$1
+MATCHID=${DEMOFILE%%_*}
+UPLOADCHECK=$(upload_check $DEMOFILE)
 
-if [ -z $uploadCheck ]
-then
-      echo "No demo found for matchid: $matchid with the filename $demofile" >> ./cleaner.log
-
-elif [ $demofile.zip == $uploadCheck ]
-then
-        echo "Match found for $demofile" >> ./cleaner.log
-        rm /home/cs/liga/serverfiles/csgo/$demofile
-        rm /home/cs/liga/serverfiles/csgo/ebot_$matchid_*
+if [ $UPLOADCHECK == 1 ]; then
+  echo "$TIMESTAMP No demo no cleaning! Missing the Demofile: $DEMOFILE - Need a human to clean it for me! :-)" >> $LOGFILE
+elif [ $UPLOADCHECK == 0 ]; then
+  echo "$TIMESTAMP $DEMOFILE was found online! Removing the old $DEMOFILE file and backup files to avoid clutter" >> $LOGFILE
+  COUNT=$(rm -vf $CSGOPATH/$DEMOFILE | wc -l)
+  echo "$TIMESTAMP Removed $COUNT demo file named $DEMOFILE" >> $LOGFILE
+  COUNT=$(rm -vf $CSGOPATH/ebot_${MATCHID}_* | wc -l)
+  echo "$TIMESTAMP Removed $COUNT backup files" >> $LOGFILE
+else
+  echo "$TIMESTAMP Not sure why I ended up here! File: cleaner.sh Demofile: $DEMOFILE Matchid: $MATCHID and Uploadcheck: $UPLOADCHECK" >> $LOGAFILE
 fi
