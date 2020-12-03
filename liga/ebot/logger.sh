bc930b26f5b5d95f43debd53cd1c6a5551bb6ed2
enghausen <morten@enghausen.dk>
2020-12-03 11:08:54 +0100

    Update logger.sh

--- liga/ebot/logger.sh
+++ liga/ebot/logger.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-source $SCRIPTPATH/config.sh
+source /home/cs/liga/scripts/ebot/config.sh
 echo "$TIMESTAMP Started logger.sh" >> $LOGFILE
+# Check LGSM log for completed demoes and upload it to ebotweb
 multitail -EX "Completed GOTV demo \"(.*?)\"" "xargs $SCRIPTPATH/uploader.sh" --mergeall $LGSMCONSOLELOGS
