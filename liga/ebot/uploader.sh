e0a6f36c8b2a8412e022746a0996581569873c6d
enghausen <morten@enghausen.dk>
2020-12-03 11:11:26 +0100

    Update uploader.sh

--- liga/ebot/uploader.sh
+++ liga/ebot/uploader.sh
@@ -4,6 +4,7 @@ DEMOFILE=$1
 curl -F "file=@$CSGOPATH/$1" https://$DOMAIN:12360/upload
 UPLOADCHECK=$(upload_check $DEMOFILE)
 
+# Statement to upload .dem files
 if [ $UPLOADCHECK == 1 ]; then
   echo "$TIMESTAMP Upload failed for $DEMOFILE" >> $LOGFILE
 elif [ $UPLOADCHECK == 0 ]; then
