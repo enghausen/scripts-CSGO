82c6715c9bdcae39b610a7cd25b591952c879a67
enghausen <morten@enghausen.dk>
2020-12-03 11:01:50 +0100

    Update logger.sh

--- liga/ebot/logger.sh
+++ liga/ebot/logger.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source ./config.sh
+source $SCRIPTPATH/config.sh
 echo "$TIMESTAMP Started logger.sh" >> $LOGFILE
-multitail -EX "Completed GOTV demo \"(.*?)\"" "xargs ./uploader.sh" --mergeall $LGSMCONSOLELOGS
+multitail -EX "Completed GOTV demo \"(.*?)\"" "xargs $SCRIPTPATH/uploader.sh" --mergeall $LGSMCONSOLELOGS
