a249802c77939b327d716d51514ae49858ddc226
enghausen <morten@enghausen.dk>
2020-12-10 09:50:43 +0100

    Update uploader.sh

--- liga/ebot/uploader.sh
+++ liga/ebot/uploader.sh
@@ -9,6 +9,7 @@ if [ $UPLOADCHECK == 1 ]; then
   echo "$TIMESTAMP Upload failed for $DEMOFILE" >> $LOGFILE
 elif [ $UPLOADCHECK == 0 ]; then
   echo "$TIMESTAMP Upload completed for $DEMOFILE" >> $LOGFILE
+  $SCRIPTPATH/cleaner.sh $DEMOFILE
 else
   echo "$TIMESTAMP Not sure why I ended up here! File: uploader.sh Demofile: $DEMOFILE and Uploadcheck: $UPLOADCHECK" >> $LOGAFILE
 fi
