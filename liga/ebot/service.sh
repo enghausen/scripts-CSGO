f21852031817f5617ae68293d17dbc35e5406383
enghausen <morten@enghausen.dk>
2021-01-27 13:24:03 +0100

    Update service.sh

--- liga/ebot/service.sh
+++ liga/ebot/service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-source /home/cs/liga/scripts/ebot/config.sh
+source /home/cs/ebot/scripts/ebot/config.sh
 echo "$TIMESTAMP Started service.sh" >> $LOGFILE
 # Use Screen to run the logger.sh "as a service"
 screen -dmS "logger.sh" $SCRIPTPATH/logger.sh
