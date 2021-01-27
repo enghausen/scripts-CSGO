c3f593c75c17b8dda76d5435717bcd7c8ca965d1
enghausen <morten@enghausen.dk>
2021-01-27 13:40:26 +0100

    URL change

--- ebot/install.sh
+++ ebot/install.sh
@@ -2,10 +2,10 @@
 # Tested on Debian 10 and will install multitail and curl
 sudo apt-get update
 sudo apt-get -y install multitail curl
-wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/liga/ebot/cleaner.sh
-wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/liga/ebot/config.sh
-wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/liga/ebot/logger.sh
-wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/liga/ebot/service.sh
-wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/liga/ebot/uploader.sh
+wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/ebot/cleaner.sh
+wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/ebot/config.sh
+wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/ebot/logger.sh
+wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/ebot/service.sh
+wget https://raw.githubusercontent.com/enghausen/scripts-CSGO/main/ebot/uploader.sh
 chmod +x cleaner.sh config.sh logger.sh service.sh uploader.sh
 echo "Remember to change the domain and paths in config.sh. You also need to change the source path in every *.sh file to get the right path for the config.sh"
